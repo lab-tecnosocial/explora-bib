ae84c08538ac399085ecb7e0f98b136c11014c35
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-06-12 22:36:35 -0400

    feat: icono boton

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,20 @@
+import { Typography } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import SearchIcon from '@mui/icons-material/Search';
 import { useState } from 'react';
 
-export default function Search({setQuery, setMostrar}) {
+export default function Search({ setQuery, setMostrar }) {
   const [q, setQ] = useState('');
-    return (
-        <form onSubmit={(e) => {
-          e.preventDefault();
-          setQuery(q);
-          setMostrar(true);
-          }}>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setQuery(q);
+    setMostrar(true);
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
         <TextField
           id="search-bar"
           className="text"
@@ -20,8 +25,24 @@ export default function Search({setQuery, setMostrar}) {
           variant="outlined"
           placeholder="Tema..."
           size="normal"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  edge="end"
+                  onClick={handleSubmit}
+                >
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+         
           fullWidth
         />
       </form>
-    )
+      <Typography variant="subtitle2" color="text.secondary" align="right">{0} resultados</Typography>
+    </>
+
+  )
 }
