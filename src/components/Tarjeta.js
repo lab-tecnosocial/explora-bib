28809e8ed513a35bdd2aa441eb74962ba4927db1
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-06-12 22:36:03 -0400

    add: botones tarjeta

--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -1,33 +1,40 @@
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
-import CardActionArea from '@mui/material/CardActionArea';
+import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Modal from './Modal';
 
-function Tarjeta({ id, img, title, author, year, publisher, link }) {
-return (
+function Tarjeta({ id, img, title, author, year, publisher, link, type, city, topics }) {
+  return (
     <Grid item key={id} xs={12} md={6}>
-      <CardActionArea component="a" href={link}>
-        <Card sx={{ display: 'flex' }}>
-          <CardContent sx={{ flex: 1 }}>
-            <Typography component="h2" variant="h6">
-              {title}
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary">
-              {year + ' - ' + publisher}
-            </Typography>
-            <Typography variant="subtitle1" paragraph>
-              {author}
-            </Typography>
-          </CardContent>
-          <CardMedia
-            component="img"
-            sx={{ width: 160 }}
-            image={img}
-          />
-        </Card>
-      </CardActionArea>
+      <Card sx={{ display: 'flex' }}>
+        <CardContent sx={{ flex: 1 }}>
+          <Typography component="h2" variant="h6">
+            {title}
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary">
+            {year + ' - ' + publisher}
+          </Typography>
+          <Typography variant="subtitle1" paragraph>
+            {author}
+          </Typography>
+          <CardActions>
+            {/* <Button size="small" >Detalles</Button> */}
+            <Modal title={title} type={type} city={city} topics={topics}/>
+            <Button size="small" href={link} target="_blank">Descargar</Button>
+          </CardActions>
+        </CardContent>
+
+        <CardMedia
+          component="img"
+          sx={{ width: 160 }}
+          image={img}
+        />
+
+      </Card>
     </Grid>
   );
 }
